cbdbe7de259a908584a9dab924e4cde0f9b9ae91
Alex Schultz <alex.c.schultz@gmail.com>
2018-08-20 22:43:38 -0400

    working on implementing working lambda code with mxnet model.

--- deeplens-lambda-read-to-me/readToMeLambda.py
+++ deeplens-lambda-read-to-me/readToMeLambda.py
@@ -1,39 +1,40 @@
 import mo
 import os
 import traceback
-from string import ascii_letters, digits
 from threading import Thread, Timer
-from autocorrect import spell
 import logging
 from time import sleep
 import sys
-import PIL.Image as Image
-import PIL.ImageDraw as ImageDraw
-import PIL.ImageFont as ImageFont
 import awscam
 import cv2
 import greengrasssdk
 import numpy as np
-import pytesseract
 import speak
 import time
 from boto3 import client
+import imageProcessing as ip
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
 client = greengrasssdk.client('iot-data')
-
 # The information exchanged between IoT and CLOUD has
 # a topic and a message body.
 # This is the topic that this code uses to send messages to cloud
 iot_topic = '$aws/things/{}/infer'.format(os.environ['AWS_IOT_THING_NAME'])
-
 ret, frame = awscam.getLastFrame()
-jpeg = None
 Write_To_FIFO = True
 FIRST_RUN = True
-
+input_width = 300
+input_height = 300
+prob_thresh = 0.60
+outMap = {1: 'text_block'}
+model_name = "read-to-me"
+occursThreshold = 3
+error, model_path = mo.optimize(model_name, input_width, input_height)
+model = awscam.Model(model_path, {"GPU": 1})
+client.publish(topic=iot_topic, payload="Model loaded.")
+model_type = "ssd"
+jpeg = None
 
 def firstRunFunc():
     logger.info('first run')
@@ -56,55 +57,6 @@ def firstRunFunc():
         exc_type, exc_value, exc_traceback = sys.exc_info()
         traceback.print_exception(exc_type, exc_value, exc_traceback, limit=5, file=sys.stdout)
 
-
-def cleanUpTextArea(image):
-    try:
-        height, width = image.shape[:2]
-        res = cv2.resize(image, (3 * width, 3 * height), interpolation=cv2.INTER_CUBIC)
-        gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (5, 5), 0)
-        denoise = cv2.fastNlMeansDenoising(blur)
-        thresh = cv2.adaptiveThreshold(denoise, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-        blur1 = cv2.GaussianBlur(thresh, (5, 5), 0)
-        final = cv2.GaussianBlur(blur1, (5, 5), 0)
-        return final
-    except Exception:
-        print("unable to display text block")
-
-
-def ocrImage(pilImage):
-    text = pytesseract.image_to_string(pilImage, lang="eng")
-    text = ExtractAlphanumeric(text)
-    return RunSpellCheck(text)
-
-
-def ExtractAlphanumeric(InputString):
-    """Remove junk characters from OCR text output.
-    Tesseract is pretty good, but sometimes it spits out a bunch of garbage characters
-    So this function strips out any non alpha numeric characteers as well as normal punctuation marks
-    before sending it off to AWS Polly to be turned into audio
-    """
-    line = InputString.replace("\n", " ")
-    return "".join([ch for ch in line if ch in (ascii_letters + digits + " " + "-" + "!" + '?')])
-
-
-def RunSpellCheck(InputString):
-    """
-    Takes a string and attempts to correct spelling on each word
-    :param InputString:
-    :return: suggested auto corrected spelling
-    """
-    words = InputString.split(' ')
-    OutPutString = ''
-    for word in words:
-        OutPutString += spell(word) + ' '
-    return OutPutString
-
-
-def ocrTest():
-    return "getting text from tesseract"
-
-
 # create a simple class that runs on its own thread so we can publish output images
 #    to the FIFO file and view using mplayer
 class FIFO_Thread(Thread):
@@ -121,43 +73,29 @@ class FIFO_Thread(Thread):
 
         while Write_To_FIFO:
             try:
-                f.write(jpeg.tobytes())
+                if jpeg is not None:
+		    f.write(jpeg.tobytes())
             except IOError as e:
                 continue
 
 
 def greengrass_infinite_infer_run():
-    input_width = 300
-    input_height = 300
-    prob_thresh = 0.55
-    outMap = {1: 'text_block'}
-    model_name = "read-to-me"
-
-    error, model_path = mo.optimize(model_name, input_width, input_height)
-
-    model = awscam.Model(model_path, {"GPU": 1})
-    client.publish(topic=iot_topic, payload="Model loaded.")
-
-    model_type = "ssd"
-    logger.info('starting lambda')
     logger.info('first run {}'.format(FIRST_RUN))
     if FIRST_RUN:
         firstRunFunc()
     try:
         results_thread = FIFO_Thread()
         results_thread.start()
-        # you can publish an "Inference starting" message to the AWS IoT console
         client.publish(topic=iot_topic, payload="Inference starting")
-        # access the latest frame on the mjpeg stream
         ret, frame = awscam.getLastFrame()
         if ret == False:
             raise Exception("Failed to get frame from the stream")
         yscale = float(frame.shape[0] / input_height)
         xscale = float(frame.shape[1] / input_width)
-
+        occurs = 0
         doInfer = True
-        frameCount = 0
         while doInfer:
+            frameContainsText = False
             # Get a frame from the video stream
             ret, frame = awscam.getLastFrame()
             # Raise an exception if failing to get a frame
@@ -166,62 +104,67 @@ def greengrass_infinite_infer_run():
 
             # Resize frame to fit model input requirement
             frameResize = cv2.resize(frame, (input_width, input_height))
-
             # Run model inference on the resized frame
             inferOutput = model.doInference(frameResize)
-
             # Output inference result to the fifo file so it can be viewed with mplayer
             parsed_results = model.parseResult(model_type, inferOutput)[model_type]
             label = ''
-            frameContainsText = False
             text_blocks = filter(lambda x: x['prob'] > prob_thresh, parsed_results)
-            text_blocks.sort(key=lambda x: x['prob'])
             # see the order of the text blocks
-            # print(text_blocks)
-            # for this lambda, we really only want to grab the highest probable text block, so just ignore the rest
+            # for this, we really only want to grab the highest probable text block, so just ignore the rest
             if len(text_blocks) > 0:
+                obj = text_blocks[0]
+                logger.debug('number of text blocks detected: [{}]'.format(len(text_blocks)))
+		text_blocks.sort(key=lambda x: x['prob'])
                 text_blocks = text_blocks[len(text_blocks) - 1:]
-
-            roi = None
-            for obj in text_blocks:
                 frameContainsText = True
+                occurs += 1
+                logger.debug('text blocks detected')
                 xmin = int(xscale * obj['xmin']) + int((obj['xmin'] - input_width / 2) + input_width / 2)
                 ymin = int(yscale * obj['ymin'])
                 xmax = int(xscale * obj['xmax']) + int((obj['xmax'] - input_width / 2) + input_width / 2)
                 ymax = int(yscale * obj['ymax'])
-                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 165, 20), 4)
-                roi = frame[(ymin:ymax), (xmin:xmax)]
-                label += '{},{:.2f}'.format(outMap[obj['label']], obj['prob'])
-                label_show = "{}:    {:.2f}%".format(outMap[obj['label']], obj['prob'] * 100)
-                cv2.putText(frame, label_show, (xmin, ymin - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 165, 20), 4)
-                label += ','
-                label += ','
-                label += time.strftime("%Y-%m-%d %H:%M:%S")
-                label += '\n'
+                logger.debug('xmin {} xmax {} ymin {} ymax {}'.format(xmin, xmax, ymin, ymax))
+                label_show = "{}: conseq: {}:    {:.2f}%".format(outMap[obj['label']], occurs, obj['prob'] * 100)
+                if occurs >= (occursThreshold - 1):
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (121, 255, 20), 4)
+                    cv2.putText(frame, label_show, (xmin, ymin - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (121, 255, 20), 4)
+                else:
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 165, 20), 4)
+                    cv2.putText(frame, label_show, (xmin, ymin - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 165, 20), 4)
+                    
+                if occurs >= occursThreshold:
+                    try:
+                        occurs = 0
+                        logger.debug('ocr iomage')
+                        tb = ip.getRoi(frame, xmin, xmax, ymin, ymax)
+                        tb = ip.correctSkew(tb)
+                        clean = ip.cleanUpTextArea(tb)
+                        txt = ip.ocrImage(clean, extractBadChars=False, spellCheck=False)
+                        if txt == '':
+                            speak.speak('Sorry, I am unable to read the page. \nPlease try again.')
+                        else:
+                            logger.info(txt)                    
+                            speak.speak(txt)
 
-            if frameContainsText and roi is not None:
-            	frameCount += 1
-            	if frameCount >= 3:
-	                try:
-	                    clean = cleanUpTextArea(roi)
-	                    text = ocrImage(clean)
-						textEx = ExtractAlphanumeric(text)
-						textSp = RunSpellCheck(textEx)
-						speak.speak(textSp)
-	                except Exception as e:
-	                    msg = "ocr failed: " + str(e)
-	                    client.publish(topic=iot_topic, payload=msg)
+                    except Exception as e:
+                        msg = "ocr failed: " + str(e)
+                        cv2.imwrite(os.path.join(os.path.abspath(os.sep),'tmp', '{}-{}-{}-{}-{}.jpg'.format(time.strftime("%Y-%m-%d %H:%M:%S"),xmin, xmax, ymin, ymax)), tb)
+                        client.publish(topic=iot_topic, payload=msg)
+
             else:
-            	frameCount = 0
-
+                occurs = 0
 
             global jpeg
-            ret, jpeg = cv2.imencode('.jpg', frame)
+            shp = frame.shape
+            smallerFrame = cv2.resize(frame, (shp[1]/3, shp[0]/3))          
+            ret, jpeg = cv2.imencode('.jpg', smallerFrame) 
+            #ret, jpeg = cv2.imencode('.jpg', frame)
+
     except Exception as e:
-        msg = "OCR failed: " + str(e)
-        speak.speak("I'm sorry, I wasn't able to read that for some reason.")
         msg = "Lambda function failed: " + str(e)
-        client.publish(topic=iot_topic, payload=msg)
+        logger.info(msg)
+        speak.speak("I'm sorry, I wasn't able to read that for some reason.")
 
     # Asynchronously schedule this function to be run again in 15 seconds
     Timer(15, greengrass_infinite_infer_run).start()
